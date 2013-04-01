0576e3587579cf42df237c8cb3e2a38239f3c647
Nyle <nyle.rodgers@gmail.com>
2013-04-01 18:26:17 -0400

    added base for all models to import from

---
+++ scouting/models/base.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import (
+    scoped_session,
+    sessionmaker,
+    )
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+from sqlalchemy.ext.declarative import declarative_base
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
