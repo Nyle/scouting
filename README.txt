49ba45a0f758fa6bce292738dcfe151a7dc9667b
Nyle <nyle.rodgers@gmail.com>
2013-04-01 17:09:51 -0400

    created project

---
+++ README.txt
@@ -0,0 +1,14 @@
+scouting README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $venv/bin/python setup.py develop
+
+- $venv/bin/initialize_scouting_db development.ini
+
+- $venv/bin/pserve development.ini
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    'transaction',
+    'pyramid_tm',
+    'pyramid_debugtoolbar',
+    'zope.sqlalchemy',
+    'waitress',
+    ]
+
+setup(name='scouting',
+      version='0.0',
+      description='scouting',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='scouting',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = scouting:main
+      [console_scripts]
+      initialize_scouting_db = scouting.scripts.initializedb:main
+      """,
+      )
