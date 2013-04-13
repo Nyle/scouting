b1dcd0b3ae93b5b2a208efe95316b69f1602a1c8
Nyle <nyle.rodgers@gmail.com>
2013-04-12 20:43:12 -0400

    added preliminary view code

--- scouting/views/view.py
+++ scouting/views/view.py
@@ -12,14 +12,35 @@ from ..models import (
     RobotMatch
     )
 
-@view_config(route_name='view', renderer='../templates/view.pt')
+@view_config(route_name='view', renderer='../templates/view/view.pt')
 def view(request):
-    return {}
+    robots = DBSession.query(Robot).order_by(Robot.robot_number).all()
+    matches = DBSession.query(Match).order_by(Match.match_number).all()
+    return {
+        'robots':robots,
+        'matches':matches,
+        }
 
-@view_config(route_name='view_robot', renderer='../templates/view_robot.pt')
+@view_config(route_name='view_robot',
+             renderer='../templates/view/view_robot.pt')
 def view_robot(request):
-    return {}
+    robot_number = int(request.matchdict['robot_number'])
+    robot = DBSession.query(Robot).filter(
+        Robot.robot_number==robot_number).first()
+    if robot is None:
+        return HTTPFound(location=request.route_url('view'))
+    return {
+        'robot':robot,
+        }
 
-@view_config(route_name='view_match', renderer='../templates/view_match.pt')
+@view_config(route_name='view_match',
+             renderer='../templates/view/view_match.pt')
 def view_match(request):
-    return {}
+    match_number = int(request.matchdict['match_number'])
+    match = DBSession.query(Match).filter(
+        Match.match_number==match_number).first()
+    if match is None:
+        return HTTPFound(location=request.route_url('view'))
+    return {
+        'match':match,
+        }
